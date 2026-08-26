f145ac8a7886cb8008620351617c3094fa034f94
Seaavey <seaavey@gmail.com>
2026-08-26 07:07:01 +0000

    refactor(db): rename oauthSessions to OAuthSessions.ts

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,7 +2,7 @@ export * from "./apiKeys.js";
 export * from "./adminAuth.js";
 export * from "./db.js";
 export * from "./logs.js";
-export * from "./oauthSessions.js";
+export * from "./OAuthSessions.js";
 export * from "./providers.js";
 export * from "./quota.js";
 export * from "./settings.js";
