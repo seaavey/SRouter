c67ca608cd9724bb48b8b20549d21cce2f7e1e7e
Seaavey <seaavey@gmail.com>
2026-08-31 15:38:23 +0000

    chore(release): bump web APP_VERSION to 0.1.4

--- packages/constants/src/version.ts
+++ packages/constants/src/version.ts
@@ -1,5 +1,5 @@
 export const GLOBAL_VERSION = "0.1.3";
-export const APP_VERSION = "0.1.3";
+export const APP_VERSION = "0.1.4";
 export const API_VERSION = "0.1.3";
 export const CLI_VERSION = "0.1.3-rc.3";
 
